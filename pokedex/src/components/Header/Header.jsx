d856df42a781f0a7c17c445ef3eeea43fcb1a3b7
Ishmaiha <ishmaihahkim@gmail.com>
2021-07-12 19:11:57 -0300

    acho que o router esta pronto

---
+++ pokedex/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Title} from "./style"
+
+export const Headers = () => {
+    return (
+      <div >
+        <div> 
+            <Title>Teste</Title>
+            <button>lalaland</button>
+        </div>       
+      </div>
+    );
+  }
+  
