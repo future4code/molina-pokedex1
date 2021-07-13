66d6152946841a4e3aa3c5ab34b604c4fb272b20
Ishmaiha <ishmaihahkim@gmail.com>
2021-07-13 15:38:03 -0300

    botoes header funcionando

--- pokedex/src/components/Header/Header.jsx
+++ pokedex/src/components/Header/Header.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import {Title, Button, GeneralContainer} from "./style"
+import {useHistory} from "react-router"
+// import { PokemonList } from "../../pages/pokemonlist/PokemonList";
+// import { Pokedex } from "../../pages/pokedex/Pokedex";
 
 export const Header = () => {
+
+    const history = useHistory()  
+    
+    const [Screen, setScreen] = useState (false)
+  
+    const goPokedex = () => {
+      history.push ("/pokedex")
+    }
+
+    const changeScreen = (() => {
+      setScreen (!Screen)
+      if (Screen === true) {
+        return goPokedex()
+      } else {
+        return goPokemonList()
+      }
+    })
+
+    const goPokemonList = () => {
+      history.push ("/")
+    }
+
     return (
       <GeneralContainer >
+        {Screen ? goPokedex : goPokemonList}
         <Button> 
-            <button>lalaland</button>
+            <button onClick = {changeScreen}>{Screen? "Go to Pokedex" : "Back to PokemonList"}</button>
+            
         </Button>
 
         <div>
