5e628121ac58cbd1d195a7054e892756f56e6499
danilo-ac <anthony.danilo@outlook.com.br>
2021-07-13 18:41:51 -0300

    teste

--- pokedex/src/components/Header/Header.jsx
+++ pokedex/src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import {Title, Button, GeneralContainer} from "./style"
 import {useHistory} from "react-router"
+import { useLocation } from "react-router";
 // import { PokemonList } from "../../pages/pokemonlist/PokemonList";
 // import { Pokedex } from "../../pages/pokedex/Pokedex";
 
 export const Header = () => {
-
+    const location = useLocation()
     const history = useHistory()  
     
     const [Screen, setScreen] = useState (false)
@@ -29,7 +30,7 @@ export const Header = () => {
 
     return (
       <GeneralContainer >
-        {Screen ? goPokedex : goPokemonList}
+        {/* {Screen ? goPokedex : goPokemonList} */}
         <Button> 
             <button onClick = {changeScreen}>{Screen? "Go to Pokedex" : "Back to PokemonList"}</button>
             
